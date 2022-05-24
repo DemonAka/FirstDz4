38ba5cf8c90f9bd4fca2aa7b841a43ede7d4c8ab
d_akulich@outlook.com <d_akulich@outlook.com>
2022-05-24 19:05:21 +0300

    Первое задание!

--- FirstDz4/Program.cs
+++ FirstDz4/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace FirstDz4
 {
@@ -11,24 +7,25 @@ namespace FirstDz4
         static void Main(string[] args)
         {
             Console.WriteLine("Введите количество строк в массиве: ");
-            int line = Convert.ToInt32( Console.ReadLine());
+            int line = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Введите количество столбцов в массиве: ");
             int column = Convert.ToInt32(Console.ReadLine());
 
             int[,] matrix = new int[line, column];
-
+            int sum = 0;
             Random random = new Random();
 
-            for (int i = 0; i < line ; i++)
+            for (int i = 0; i < line; i++)
             {
                 for (int j = 0; j < column; j++)
                 {
-                    matrix[i,j] = random.Next(10);
+                    matrix[i, j] = random.Next(-100, 100);
+                    sum = sum + matrix[i, j];
                     Console.Write($"{matrix[i, j]} ");
                 }
                 Console.WriteLine();
             }
-            
+            Console.WriteLine($"Сумма всех чисел матрицы равна : {sum}");
             Console.ReadKey();
         }
     }
