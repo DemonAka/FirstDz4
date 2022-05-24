58b2297e7b3ba02239690127cde7b04159a90ddb
d_akulich@outlook.com <d_akulich@outlook.com>
2022-05-24 19:01:18 +0300

    Добавьте файлы проекта.

---
+++ FirstDz4/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstDz4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите количество строк в массиве: ");
+            int line = Convert.ToInt32( Console.ReadLine());
+            Console.WriteLine("Введите количество столбцов в массиве: ");
+            int column = Convert.ToInt32(Console.ReadLine());
+
+            int[,] matrix = new int[line, column];
+
+            Random random = new Random();
+
+            for (int i = 0; i < line ; i++)
+            {
+                for (int j = 0; j < column; j++)
+                {
+                    matrix[i,j] = random.Next(10);
+                    Console.Write($"{matrix[i, j]} ");
+                }
+                Console.WriteLine();
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
